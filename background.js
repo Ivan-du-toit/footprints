09ce79bce42b1f70b5376a583c6bbd5539fa5e98
Ivan du Toit <anilihst@gmail.com>
2013-12-16 18:42:50 +0200

    Made the plugin work with a solr server running on localhost.

--- background.js
+++ background.js
@@ -1,14 +1,29 @@
-//Background
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 	
 	if (changeInfo.status == 'complete') {
-		console.log('Opened: ' + tab.url);
+		if (tab.url.indexOf('localhost') > -1 || tab.url.indexOf('.google.') > -1) {
+			console.log('Opened: ' + tab.url);
+			console.log('not logging');
+			return;
+		}
 		var url = tab.url;
 		chrome.tabs.sendRequest(tab.id, {method: 'getText'}, function(response) {
+		
 			if(response.method=="getText"){
-				console.log('Text: ' + response.data);
+				var data = {
+					"add": {
+						"doc":{
+							"id": url,
+							"url": url,
+							"content": response.data
+						},
+						"boost":1.0,
+						"overwrite":true,
+						"commitWithin":1000
+					}
+				};
 				
-				var data = 'URL=' +  encodeURIComponent(url) + '&data=' + encodeURIComponent(response.data);
+				
 				
 				var xhr = new XMLHttpRequest();
 				xhr.onreadystatechange = function(){
@@ -19,9 +34,9 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 					}
 				};
 				
-				xhr.open('POST', "http://localhost/test/dump.php", true);
-				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xhr.send(data);
+				xhr.open('POST', "http://localhost:8983/solr/collection1/update?wt=json", true);
+				xhr.setRequestHeader("Content-type", "application/json");
+				xhr.send(JSON.stringify(data));
 			}
 		});
 	}
