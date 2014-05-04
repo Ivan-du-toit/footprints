faeadbe1efed8e9d6dce0ff328efcc0026a93c50
Ivan du Toit <anilihst@gmail.com>
2014-05-04 18:56:21 +0200

    Added sqlx for DB management, gorest for restful API and a json parser for config management.

--- server/server.go
+++ server/server.go
@@ -1,54 +1,108 @@
 package main
 
 import (
-    "database/sql"
+    "encoding/json"
     "fmt"
-    "github.com/codegangsta/martini"
-    //"github.com/jmoiron/sqlx"
+    "github.com/emicklei/go-restful"
+    "github.com/emicklei/go-restful/swagger"
+    "github.com/jmoiron/sqlx"
     _ "github.com/lib/pq"
+    "io/ioutil"
     "log"
-    "os"
+    "net/http"
 )
 
 type Page struct {
-    url     string
-    content string
+    Id      int
+    Title   string `db:"title"`
+    Url     string
+    Content string
 }
 
-func setup() {
-    //TODO:Swap the loggin out for go-logging or log4go, log4go might be ther best choice
-    log.SetOutput(os.Stdout)
-    log.Println("Start logging")
-
+type Config struct {
+    DBName     string
+    DBUser     string
+    DBPassword string
 }
 
-//TODO: After experimentation follow a BDD approach
-func main() {
-    db, err := sql.Open("postgres", "user=psql dbname=footprints password=Anilihst1 host=localhost")
+type PageService struct {
+    pages map[string]Page
+}
+
+func (page PageService) Register() {
+    ws := new(restful.WebService)
+    ws.
+        Path("/pages").
+        Consumes(restful.MIME_XML, restful.MIME_JSON).
+        Produces(restful.MIME_JSON, restful.MIME_XML)
+
+    ws.Route(ws.GET("/{page-id}").To(page.FindPage).
+        //This is just docs
+        Doc("get a page").
+        Operation("findPage").
+        Param(ws.PathParameter("page-id", "identifier of the page").DataType("integer")).
+        Writes(Page{}))
+
+    restful.Add(ws)
+}
+
+func (ps PageService) FindPage(request *restful.Request, response *restful.Response) {
+    id := request.PathParameter("page-id")
+    page := ps.pages[id]
+
+    if page.Id == 0 {
+        response.WriteErrorString(http.StatusNotFound, "Page could not be found.")
+    } else {
+        response.WriteEntity(page)
+    }
+}
+
+func dbSetup(config Config, pageService PageService) {
+    db, err := sqlx.Open("postgres", "user="+config.DBUser+
+        " dbname="+config.DBName+
+        " password="+config.DBPassword+" host=localhost")
     if err != nil {
         log.Fatal(err)
     }
     defer db.Close()
 
-    //TODO:Implement the sqlx struct mapping
-    rows, err := db.Query("SELECT * FROM pages")
+    pages := []Page{}
+    err = db.Select(&pages, "SELECT * FROM page")
     if err != nil {
-        log.Fatal(err)
+        fmt.Printf("err", err)
+        return
     }
-    var pageId int
-    var url string
-    rows.Next()
-    rows.Scan(&pageId, &url)
-    fmt.Println("Rows:", rows)
-    fmt.Println("Fields:", pageId)
-    fmt.Println("Url: ", url)
 
-    //config := jsonConfig.LoadConfig()
-    //fmt.Println("Config: ", config)
+    pageService.pages["1"] = pages[0]
+    fmt.Print("%#v\n", pageService.pages["1"])
+}
 
-    m := martini.Classic()
-    m.Get("/", func() string {
-        return "Hello there rows found"
-    })
-    m.Run()
+func main() {
+    pageService := PageService{map[string]Page{}}
+    pageService.Register()
+
+    var config Config
+    configFile, err := ioutil.ReadFile("config.json")
+    if err != nil {
+        log.Fatal("opening config file. ", err.Error())
+    }
+
+    err = json.Unmarshal(configFile, &config)
+    if err != nil {
+        log.Fatal("Error parsing config file. ", err.Error())
+    }
+
+    swaggerConfig := swagger.Config{
+        WebServices:    restful.RegisteredWebServices(), // you control what services are visible
+        WebServicesUrl: "http://localhost:8080",
+        ApiPath:        "/apidocs.json",
+        // Optionally, specifiy where the UI is located
+        SwaggerPath:     "/apidocs/",
+        SwaggerFilePath: "swagger"}
+    swagger.InstallSwaggerService(swaggerConfig)
+
+    dbSetup(config, pageService)
+
+    log.Printf("start listening on localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
