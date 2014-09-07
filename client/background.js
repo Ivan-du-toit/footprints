614ff7940b7900793809d1572ab488a4100ff5bc
Ivan du Toit <anilihst@gmail.com>
2014-09-07 20:41:00 +0200

    Update the client extension to use the new restful server format and location.

--- client/background.js
+++ client/background.js
@@ -1,40 +1,50 @@
+var DEBUG = true;
+var SERVER_URL = 'http://192.168.1.102:8888/';
+var BLACK_LIST = ['localhost', 'google', 'amazon'];
+
+//Check to see if URL is blacklisted.
+//This function should be improved to have a dynamically saved blacklist. Possibly pulled from the server.
+function shouldCaptureURL(url) {
+	for (var index in BLACK_LIST) {
+		if (url.match(BLACK_LIST[index]) !== null) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function debugLog(message) {
+	if (DEBUG === true) {
+		console.log(message)
+	}
+}
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-	
+
 	if (changeInfo.status == 'complete') {
-		if (tab.url.indexOf('localhost') > -1 || tab.url.indexOf('.google.') > -1) {
-			console.log('Opened: ' + tab.url);
-			console.log('not logging');
+		if (!shouldCaptureURL(tab.url)) {
+		//if (tab.url.indexOf('localhost') > -1 || tab.url.indexOf('.google.') > -1) {
+			debugLog("Black listed URL encountered. Not capturing: " + tab.url)
 			return;
 		}
-		var url = tab.url;
 		chrome.tabs.sendRequest(tab.id, {method: 'getText'}, function(response) {
-		
 			if(response.method=="getText"){
 				var data = {
-					"add": {
-						"doc":{
-							"id": url,
-							"url": url,
-							"content": response.data
-						},
-						"boost":1.0,
-						"overwrite":true,
-						"commitWithin":1000
-					}
+					"Content": response.data,
+					"Title":tab.title,
+					"Url":tab.url
 				};
-				
-				
-				
+
 				var xhr = new XMLHttpRequest();
 				xhr.onreadystatechange = function(){
-					if(xhr.readyState == 4 && xhr.status == 200) {
+					if (xhr.readyState == 4 && ( xhr.status == 200 || xhr.status == 201)) {
 						console.log('Data sent: ' + xhr.responseText);
 					} else if (xhr.readyState == 4) {
 						console.log('Error: ' + xhr.responseText);
 					}
 				};
-				
-				xhr.open('POST', "http://localhost:8983/solr/collection1/update?wt=json", true);
+
+				xhr.open('POST', SERVER_URL+ "pages", true);
 				xhr.setRequestHeader("Content-type", "application/json");
 				xhr.send(JSON.stringify(data));
 			}
--- client/content.js
+++ client/content.js
@@ -1,7 +1,17 @@
 chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
+    function(request, sender, callback) {
         if(request.method == "getText"){
-            sendResponse({data: document.all[0].innerText, method: "getText"}); //same as innerText
+            callback({data: document.all[0].innerText, method: "getText"}); //same as innerText
         }
     }
 );
+
+// Warn if the browser doesn't support addEventListener or the Page Visibility API
+if (typeof document.addEventListener === "undefined" || typeof hidden === "undefined") {
+	console.log('Replace  this to return the content if it does not work!!!')
+//	alert("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+} else {
+	// Handle page visibility change   
+	console.log('The page is being viewed. Send the content');
+	//document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}
